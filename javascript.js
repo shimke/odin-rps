da58b43a73988fc5ba2ec49bfb0e15047aa43bc3
shimke <127237359+shimke@users.noreply.github.com>
2024-07-09 23:56:42 -0400

    Update human choice fn to return integer
    This will let the game round logic just use math and avoid handling strs

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//declare integer consts for each choice
+
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3);
     console.log(`Computer chose ${randomChoice}`);
@@ -5,8 +7,23 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    const humanChoice = prompt("Enter your throw: Rock, paper, or scissors");
-    console.log(`Human chose ${humanChoice}`);
+    const humanInput = prompt("Enter your throw: rock, paper, or scissors");
+    console.log(`Human chose ${humanInput}`);
+    let humanChoice;
+    switch(humanInput) {
+        case "rock":
+            humanChoice = 0;
+            break
+        case "paper":
+            humanChoice = 1;
+            break;
+        case "scissors":
+            humanChoice = 2;
+            break
+        default:
+            console.log("Sorry, that's not a valid choice!");
+    }
+
     return humanChoice;
 }
 
