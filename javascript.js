720f626cfeabe0d1f761da37baa8b9cffeb42619
shimke <127237359+shimke@users.noreply.github.com>
2024-07-21 17:10:38 -0400

    Create playGame fn and move other fns inside it

--- javascript.js
+++ javascript.js
@@ -8,51 +8,62 @@ const throwLookup = {
     2: 'scissors'
 }
 
-//declare score variables
-let computerScore = 0;
-let humanScore = 0;
+function playGame() {
+    //declare score variables
+    let computerScore = 0;
+    let humanScore = 0;
 
-function getComputerChoice() {
-    const randomChoice = throwLookup[Math.floor(Math.random() * 3)];
-    console.log(`Computer chose ${randomChoice}`);
-    return randomChoice;
-}
+    for (i=0; i<6; i++) {
+        let compThrow = getComputerChoice();
+        let humanThrow = getHumanChoice();
 
-function getHumanChoice() {
-    const humanChoice = prompt("Enter your throw: rock, paper, or scissors").toLowerCase();
+        playRound(compThrow,humanThrow);
+    }
+
+    function playRound(compChoice,humanChoice) {
+        let compThrow = throwLookup[compChoice];
+        let userThrow = throwLookup[humanChoice];
     
-    switch(humanChoice) {
-        case "rock":
-        case "paper":
-        case "scissors":
-            console.log(`Human chose ${humanChoice}`);
-            return humanChoice;
-            break;
-        default:
-            console.log("Sorry, that's not a valid choice! Please try again.");
+        //calculates outcome - Adding 3 to difference ensures remainder is positive
+        let outcome = ((userThrow - compThrow) + 3) % 3
+    
+        //Map outcome to score - Outcome of 0 is tie, 1 is human win, 2 is human loss
+        switch (outcome) {
+            case 1:
+                humanScore++;
+                console.log(`You win! You threw ${humanChoice} which beats ${compChoice}!`);
+                break;
+            case 2:
+                computerScore++;
+                console.log(`You lose! You threw ${humanChoice} which loses to ${compChoice}!`);
+                break;
+        }
+    
+    }
 
-            //This recursion is ok because it's just a demo browser game and moreover is limited by human input. 
-            return getHumanChoice();
+    function getComputerChoice() {
+        const randomChoice = throwLookup[Math.floor(Math.random() * 3)];
+        console.log(`Computer chose ${randomChoice}`);
+        return randomChoice;
+    }
+    
+    function getHumanChoice() {
+        const humanChoice = prompt("Enter your throw: rock, paper, or scissors").toLowerCase();
+        
+        switch(humanChoice) {
+            case "rock":
+            case "paper":
+            case "scissors":
+                console.log(`Human chose ${humanChoice}`);
+                return humanChoice;
+                break;
+            default:
+                console.log("Sorry, that's not a valid choice! Please try again.");
+    
+                //This recursion is ok because it's just a demo browser game and moreover is limited by human input. 
+                return getHumanChoice();
+        }
     }
 }
 
-function playRound(compChoice,humanChoice) {
-    let compThrow = throwLookup[compChoice];
-    let userThrow = throwLookup[humanChoice];
 
-    //calculates outcome - Adding 3 to difference ensures remainder is positive
-    let outcome = ((userThrow - compThrow) + 3) % 3
-
-    //Map outcome to score - Outcome of 0 is tie, 1 is human win, 2 is human loss
-    switch (outcome) {
-        case 1:
-            humanScore++;
-            console.log(`You win! You threw ${humanChoice} which beats ${compChoice}!`);
-            break;
-        case 2:
-            computerScore++;
-            console.log(`You lose! You threw ${humanChoice} which loses to ${compChoice}!`);
-            break;
-    }
-
-}
